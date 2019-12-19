291d6eec29399694a1104cce4faeeb31ff556312
Will Hinkelman <will.hink24@gmail.com>
2019-12-19 14:12:22 -0800

    photo upload

--- main_app/views.py
+++ main_app/views.py
@@ -55,18 +55,13 @@ class RecordDelete(LoginRequiredMixin, DeleteView):
 
 @login_required
 def add_photo(request, record_id):
-	# photo-file was the "name" attribute on the <input type="file">
   photo_file = request.FILES.get('photo-file', None)
   if photo_file:
     s3 = boto3.client('s3')
-    # need a unique "key" for S3 / needs image file extension too
     key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-    # just in case something goes wrong
     try:
       s3.upload_fileobj(photo_file, BUCKET, key)
-      # build the full url string
       url = f"{S3_BASE_URL}{BUCKET}/{key}"
-      # we can assign to cat_id or cat (if you have a cat object)
       photo = Photo(url=url, record_id=record_id)
       photo.save()
     except:
