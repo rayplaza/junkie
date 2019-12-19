98ef971ea0e2122264e70bc22f02870f644aa477
Will Hinkelman <will.hink24@gmail.com>
2019-12-19 13:38:08 -0800

    add_photo function

--- main_app/views.py
+++ main_app/views.py
@@ -5,8 +5,13 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Record
+from .models import Record, Photo
+import uuid
+import boto3
 # Create your views here.
+
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'catcollector'
 
 def home(request):
     return render(request, 'home.html')
@@ -47,3 +52,23 @@ class RecordUpdate(LoginRequiredMixin, UpdateView):
 class RecordDelete(LoginRequiredMixin, DeleteView):
   model = Record
   success_url = '/records/'
+
+@login_required
+def add_photo(request, record_id):
+	# photo-file was the "name" attribute on the <input type="file">
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # need a unique "key" for S3 / needs image file extension too
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    # just in case something goes wrong
+    try:
+      s3.upload_fileobj(photo_file, BUCKET, key)
+      # build the full url string
+      url = f"{S3_BASE_URL}{BUCKET}/{key}"
+      # we can assign to cat_id or cat (if you have a cat object)
+      photo = Photo(url=url, record_id=record_id)
+      photo.save()
+    except:
+      print('An error occurred uploading file to S3')
+  return redirect('records_detail', pk=record_id)
