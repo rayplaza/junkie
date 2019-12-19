a0f80952981b7fa40a32b23fa645a7f713e9f4c9
Will Hinkelman <will.hink24@gmail.com>
2019-12-18 20:11:28 -0800

    update and delete record views

--- main_app/views.py
+++ main_app/views.py
@@ -14,18 +14,13 @@ def home(request):
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in via code
       login(request, user)
       return redirect('home')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
@@ -40,3 +35,10 @@ class RecordCreate(LoginRequiredMixin, CreateView):
     model = Record
     fields = '__all__'
 
+class RecordUpdate(LoginRequiredMixin, UpdateView):
+  model = Record
+  fields = ['album_name', 'artist', 'year', 'label', 'condition']
+
+class RecordDelete(LoginRequiredMixin, DeleteView):
+  model = Record
+  success_url = '/records/'
